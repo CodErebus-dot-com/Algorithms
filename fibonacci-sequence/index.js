26bf74290ee382dd7f290efcee790ee950495858
VS <varkisebi@gmail.com>
2022-06-13 14:46:24 +0530

    fibonacci-sequence

--- fibonacci-sequence/index.js
+++ fibonacci-sequence/index.js
@@ -1,13 +1,21 @@
+const initialValue = 0;
+
 function fibonacci(n) {
   const fib = [0, 1];
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
-  return fib;
+
+  const sum = fib.reduce(
+    (prevValue, currValue) => prevValue + currValue,
+    initialValue
+  );
+  console.log(`Sequence for n = ${n}: `, fib);
+  console.log(`Sum for n = ${n}: `, sum);
 }
 
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(7));
+fibonacci(2);
+fibonacci(3);
+fibonacci(7);
 
 // Big-O = O(n)
